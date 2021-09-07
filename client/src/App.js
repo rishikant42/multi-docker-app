acd4ebd8b46981c5d7f660a0e608bd5ff62266dc
Rishi kant <rshkntshrm@gmail.com>
2021-09-07 21:14:39 +0530

    multi docker app

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div>
+          <br/><br/>
+          <Link to="/">Home</Link>&nbsp;&nbsp;
+          <Link to="/otherpage">Other Page link</Link><br/><br/>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
